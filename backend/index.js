4fef7357af4f683badaeadfc972a918d0153e267
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-08-25 22:54:46 +0530

    setting up backend structure

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose
+    .connect(
+        "mongoDB-URL",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    ).then((x) => {
+        console.log("connected to mongoDB")
+    })
+    .catch((err) => {
+        console.log("Error while connecting to mongoDB")
+    });
+
+
+app.get('/', (req, res) => {
+    res.send("Hello, world!");
+})
+
+
+app.listen(port, () => {
+    console.log("App listening on port " + port)
+})
