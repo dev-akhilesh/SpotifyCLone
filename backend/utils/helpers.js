5bee4c0e9152adbe6e6f8a43800f99c79de50ef8
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-08-28 22:04:14 +0530

    helper function og getToken

---
+++ backend/utils/helpers.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+exports = {};
+
+exports.getToken = async (email, user) => {
+    // Assume this code is complete
+    const token = jwt.sign(
+        { identifier: user._id },
+        process.env.SECRET_KEY_PASSPORT
+    );
+    return token;
+};
+
+module.exports = exports;
