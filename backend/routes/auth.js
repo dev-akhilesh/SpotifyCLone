737f7f39fca6c59d8710f54bd9cbe43511bfa1e9
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-08-28 22:06:54 +0530

    register route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,4 +2,48 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
+// This POST route will help to register a user
+router.post("/register", async (req, res) => {
+    // This code is run when the /register api is called as a POST request
+
+    // My req.body will be of the format {email, password, firstName, lastName, username }
+    const { email, password, firstName, lastName, username } = req.body;
+
+    // Step 2 : Does a user with this email already exist? If yes, we throw an error.
+    const user = await User.findOne({ email: email });
+    if (user) {
+        // status code by default is 200
+        return res
+            .status(403)
+            .json({ error: "A user with this email already exists" });
+    }
+    // This is a valid request
+
+    // Step 3: Create a new user in the DB
+    // Step 3.1 : We do not store passwords in plain text.
+    // xyz: we convert the plain text password to a hash.
+    // xyz --> asghajskbvjacnijhabigbr
+    // My hash of xyz depends on 2 parameters.
+    // If I keep those 2 parameters same, xyz ALWAYS gives the same hash.
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUserData = {
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+        username,
+    };
+    const newUser = await User.create(newUserData);
+    console.log(newUserData);
+
+    // Step 4: We want to create the token to return to the user
+    const token = await getToken(email, newUser);
+
+    // Step 5: Return the result to the user
+    const userToReturn = { ...newUser.toJSON(), token };
+    console.log(userToReturn);
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+});
+
 module.exports = router;
