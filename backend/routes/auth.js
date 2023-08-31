e56cb604502c23b6cb08c920149b30c3ea792ed7
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-01 04:56:25 +0530

    auth routes done(login,register)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const { getToken } = require("../utils/helpers");
 
-// This POST route will help to register a user
+// This POST route will help to register a user.
 router.post("/register", async (req, res) => {
     // This code is run when the /register api is called as a POST request
 
@@ -42,6 +44,36 @@ router.post("/register", async (req, res) => {
     // Step 5: Return the result to the user
     const userToReturn = { ...newUser.toJSON(), token };
     console.log(userToReturn);
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+});
+
+// This POST route will help to login a user
+router.post("/login", async (req, res) => {
+    // Step 1: Get email and password sent by user from req.body
+    const { email, password } = req.body;
+
+    // Step 2: Check if a user with the given email exists. If not, the credentials are invalid.
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(403).json({ err: "Invalid credentials" });
+    }
+
+    console.log(user);
+
+    // Step 3: If the user exists, check if the password is correct. If not, the credentials are invalid.
+    // This is a tricky step. Why? Because we have stored the original password in a hashed form, which we cannot use to get back the password.
+    // I cannot do : if(password === user.password)
+    // bcrypt.compare enabled us to compare 1 password in plaintext(password from req.body) to a hashed password(the one in our db) securely.
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    // This will be true or false.
+    if (!isPasswordValid) {
+        return res.status(403).json({ err: "Invalid credentials" });
+    }
+
+    // Step 4: If the credentials are correct, return a token to the user.
+    const token = await getToken(user.email, user);
+    const userToReturn = { ...user.toJSON(), token };
     delete userToReturn.password;
     return res.status(200).json(userToReturn);
 });
