822d04e732b7d26be9ea6e75c363e0092cefb4fd
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-01 04:57:58 +0530

    create song route

--- backend/routes/song.js
+++ backend/routes/song.js
@@ -2,4 +2,24 @@ const express = require("express");
 const router = express.Router();
 const Song = require("../models/Song");
 
+router.post(
+    "/create",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        // req.user getss the user because of passport.authenticate
+        const { name, thumbnail, track } = req.body;
+        if (!name || !thumbnail || !track) {
+            return res
+                .status(301)
+                .json({ err: "Insufficient details to create song." });
+        }
+        const artist = req.user._id;
+        const songDetails = { name, thumbnail, track, artist };
+        const createdSong = await Song.create(songDetails);
+        return res.status(200).json(createdSong);
+    }
+);
+
+
+
 module.exports = router;
