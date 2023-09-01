420b61bbdbad9d9d3970c8c56974232b73f8034f
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-01 23:26:48 +0530

    playlist route

--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -1,5 +1,54 @@
 const express = require("express");
-const router = express.Router();
+const passport = require("passport");
 const Playlist = require("../models/Playlist");
+const User = require("../models/User");
+const Song = require("../models/Song");
+
+const router = express.Router();
+
+// Route 1: Create a playlist
+router.post(
+    "/create",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const currentUser = req.user;
+        const { name, thumbnail, songs } = req.body;
+        if (!name || !thumbnail || !songs) {
+            return res.status(301).json({ err: "Insufficient data" });
+        }
+        const playlistData = {
+            name,
+            thumbnail,
+            songs,
+            owner: currentUser._id,
+            collaborators: [],
+        };
+        const playlist = await Playlist.create(playlistData);
+        return res.status(200).json(playlist);
+    }
+);
+
+// Route 2: Get a playlist by ID
+// we will get the playlist ID as a route parameter and we will return teh playlist having that id
+// /something1/something2/something3 --> exact match
+// /something1/something2/something4 --> this will not call the api on the previus line
+// If we are doing /playlist/get/:playlistId (focus on teh :) --> this means that playlistId is now a variable to which we can assign any value
+// If you call anything of the format /playlist/get/asdvniuen (asdvniuen can be anything), this api is called
+// If you called /playlist/get/asdvniuen, the playlistId variable gets assigned teh value asdvniuen.
+router.get(
+    "/get/playlist/:playlistId",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        // This concept is called req.params
+        const playlistId = req.params.playlistId;
+        // I need to find a playlist with the _id = playlistId
+        const playlist = await Playlist.findOne({ _id: playlistId });
+        if (!playlist) {
+            return res.status(301).json({ err: "Invalid ID" });
+        }
+        return res.status(200).json(playlist);
+    }
+);
+
 
 module.exports = router;
--- backend/routes/song.js
+++ backend/routes/song.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 const Song = require("../models/Song");
+const User = require("../models/User");
 
 // Post route to create a song.
 router.post(
@@ -33,5 +35,43 @@ router.get(
         return res.status(200).json({ data: songs });
     }
 );
+
+// Get route to get all songs any artist has published
+// I will send the artist id and I want to see all songs that artist has published.
+router.get(
+    "/get/artist/:artistId",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const { artistId } = req.params;
+        // We can check if the artist does not exist
+        const artist = await User.findOne({ _id: artistId });
+        // ![] = false
+        // !null = true
+        // !undefined = true
+        if (!artist) {
+            return res.status(301).json({ err: "Artist does not exist" });
+        }
+
+        const songs = await Song.find({ artist: artistId });
+        return res.status(200).json({ data: songs });
+    }
+);
+
+// Get route to get a single song by name
+router.get(
+    "/get/songname/:songName",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        const { songName } = req.params;
+
+        // name:songName --> exact name matching. Vanilla, Vanila
+        // Pattern matching instead of direct name matching.
+        const songs = await Song.find({ name: songName }).populate("artist");
+        return res.status(200).json({ data: songs });
+    }
+);
+
+module.exports = router;
+
 
 module.exports = router;
