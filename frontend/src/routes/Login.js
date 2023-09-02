f3f9e2f42ed0668f32ba36a9ea97d6fc71f72bf5
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-02 21:17:20 +0530

    login page

---
+++ frontend/src/routes/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Icon } from "@iconify/react";
+
+const Login = () => {
+    return (
+        <div className="w-full h-full flex flex-col items-center">
+            <div className="logo p-5 border-b border-solid border-gray-300 w-full flex justify-center">
+                <Icon icon="logos:spotify" width="150" />
+            </div>
+            <div className="inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
+                {/*  I will have my 2 inputs(email and password) and I will have my sign up instead button*/}
+            
+            </div>
+        </div>
+    )
+}
+
+export default Login
