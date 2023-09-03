ad599d0a07545436868ed86e41c2dc8829b34a73
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-03 21:48:10 +0530

    signup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './output.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from './routes/Login';
+import Signup from './routes/Signup';
 
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen font-poppins">
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<>homepage</>} />
           <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Icon } from "@iconify/react";
+import TextInput from "../components/shared/TextInput";
+import PasswordInput from "../components/shared/PasswordInput";
 
 const Login = () => {
     const [email, setEmail] = useState("");
