f7ca4c6e5d17d85452d2b8106e36a5ad6b9bd49e
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-08 01:06:34 +0530

    my-songs

--- frontend/src/routes/UploadSong.js
+++ frontend/src/routes/UploadSong.js
@@ -1,12 +1,12 @@
-import {useState} from "react";
-import {Icon} from "@iconify/react";
+import { useState } from "react";
+import { Icon } from "@iconify/react";
 import spotify_logo from "../assets/images/spotify_logo_white.svg";
 import CloudinaryUpload from "../components/shared/CloudinaryUpload";
 import IconText from "../components/shared/IconText";
 import TextInput from "../components/shared/TextInput";
 import TextWithHover from "../components/shared/TextWithHover";
-import {makeAuthenticatedPOSTRequest} from "../utils/serverHelpers";
-import {useNavigate} from "react-router-dom";
+import { makeAuthenticatedPOSTRequest } from "../utils/serverHelpers";
+import { useNavigate } from "react-router-dom";
 
 const UploadSong = () => {
     const [name, setName] = useState("");
@@ -16,7 +16,7 @@ const UploadSong = () => {
     const navigate = useNavigate();
 
     const submitSong = async () => {
-        const data = {name, thumbnail, track: playlistUrl};
+        const data = { name, thumbnail, track: playlistUrl };
         const response = await makeAuthenticatedPOSTRequest(
             "/song/create",
             data
