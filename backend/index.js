ddf0b055f9a898650c5f0bb8dd9cc404207b153d
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-07 21:24:20 +0530

    jwt changes

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET_KEY_PASSPORT;
 
 passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+    User.findOne({ _id: jwt_payload.identifier }, function (err, user) {
         if (err) {
             return done(err, false);
         }
