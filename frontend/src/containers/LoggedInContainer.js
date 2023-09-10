975e0aa359bba0dfd7f021a7c87708b9a678b8a9
dev-akhilesh <akhileshtakawale703@gmail.com>
2023-09-10 20:12:46 +0530

    updated navbar

--- frontend/src/containers/LoggedInContainer.js
+++ frontend/src/containers/LoggedInContainer.js
@@ -179,7 +179,7 @@ const LoggedInContainer = ({ children, curActiveScreen }) => {
                             <div className="w-1/3 flex justify-around h-full items-center">
                                 <TextWithHover displayText={"Upload Song"} />
                                 <div className="bg-white w-10 h-10 flex items-center justify-center rounded-full font-semibold cursor-pointer">
-                                    AC
+                                    AT
                                 </div>
                             </div>
                         </div>
